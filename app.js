fe91e9ecbc1a701ca1cbe44483f292886b232589
peacepiece7 <peacepiece7@naver.com>
2023-04-10 22:00:09 +0900

    feat : movie api 추가

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+const axios = require("axios")
 const express = require("express")
 
 const app = express()
 
-app.use("/", (res,req) => {
+app.use(express.json())
+app.use(express.text())
+
+app.use("/movies", async (req,res) => {
+    try{
+        const title = req.query.title
+        const {data} = await axios({
+            url : `https://omdbapi.com/?apikey=7035c60c&s=${title}`,
+            method : "GET"
+        })
+        console.log(data.Search)
+        res.status(202).json(data.Search)
+    }catch(err){
+        console.error(err)
+        res.status(404).send("404 not found title query string을 입력해주세요!")
+    }
+})
+
+app.use("/", (req,res) => {
     console.log("Hello, world!")
-    req.status(202).send("Hi Ubunto")
+    res.status(202).send("Hi Ubunto")
 })
 
 app.listen(6060, () => {
