e6425ff3dd0c65def6a578e47a915cfcbd1d3fea
peacepiece7 <peacepiece7@naver.com>
2023-04-11 23:04:49 +0900

    feat : html 빌드 파일 보여주는 라우터 추가
    - summakjo netlify같은 호스팅 사이트 기본적인 작동방법 알려주기 위한 예제

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const axios = require("axios")
-const express = require("express")
+const axios = require('axios')
+const express = require('express')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.text())
 
-app.use("/movies", async (req,res) => {
-    try{
-        const title = req.query.title
-        const {data} = await axios({
-            url : `https://omdbapi.com/?apikey=7035c60c&s=${title}`,
-            method : "GET"
-        })
-        console.log(data.Search)
-        res.status(202).json(data.Search)
-    }catch(err){
-        console.error(err)
-        res.status(404).send("404 not found title query string을 입력해주세요!")
-    }
+app.use('/movies', async (req, res) => {
+  try {
+    const title = req.query.title
+    const { data } = await axios({
+      url: `https://omdbapi.com/?apikey=7035c60c&s=${title}`,
+      method: 'GET',
+    })
+    console.log(data.Search)
+    res.status(202).json(data.Search)
+  } catch (err) {
+    console.error(err)
+    res.status(404).send('404 not found title query string을 입력해주세요!')
+  }
 })
 
-app.use("/", (req,res) => {
-    console.log("Hello, world!")
-    res.status(202).send("Hi Ubunto")
+app.use('/', (req, res) => {
+  console.log('Hello, world!')
+  res.status(202).send('<h1>Hello, world</h1>')
+  console.log('Hello, Im GW')
+  console.log('hello, everyone')
 })
 
 app.listen(6060, () => {
-    console.log("Express Server가 연결 되었습니다.")
+  console.log('Express Server가 연결 되었습니다.')
 })
